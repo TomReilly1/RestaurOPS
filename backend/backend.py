b23b05f6ce01c79a7f2ff6aa150a97d83374541f
TomReilly1 <reilly.thomas.c@gmail.com>
2022-03-31 12:37:12 -0400

    api handles array of obj instead of individual obj

--- backend/backend.py
+++ backend/backend.py
@@ -1,24 +1,62 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from datetime import datetime
 
 
 ######### CONFIG #########
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@127.0.0.1:3306/restops'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@127.0.0.1:3306/restops'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://tom:Jkll233-=@192.168.122.90:3306/restops'
 db = SQLAlchemy(app)
 
 
 ######### MODELS #########
-class Food(db.Model):
-	__tablename__ = 'food_orders'
+class OrderItems(db.Model):
+	__tablename__ = 'order_items'
 
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(100), nullable=False)
+	# order_item_id = db.Column(db.Integer, primary_key=True)
+	# order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id'))
+	# item_id = db.Column(db.Integer, db.ForeignKey('items.item_id'))
+	# price = db.Column(db.Float, db.ForeignKey('items.price'))
+
+	order_item_id = db.Column(db.Integer, primary_key=True)
+	order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id'))
+	name = db.Column(db.String(), nullable=False)
 	price = db.Column(db.Float, nullable=False)
 
 	def __init__(self, name, price):
+		self.name = name
+		self.price = price
+
+class Orders(db.Model):
+	__tablename__ = 'orders'
+
+	order_id = db.Column(db.Integer, primary_key=True)
+	order_time = db.Column(db.DateTime)
+
+	def __init__(self, order_time):
+		self.order_time = order_time
+
+	def __repr__(self):
+		return f'{self.order_id}, {self.order_time}'
+# class Taxes(db.Model):
+# 	__tablename__ = 'taxes'
+
+# 	tax_id = db.Column(db.Integer, primary_key=True)
+# 	order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id'))
+# 	tax_amount = db.Column(db.Numeric(2, 10))
+
+class Items(db.Model):
+	__tablename__ = 'items'
+
+	item_id = db.Column(db.String(100), primary_key=True)
+	name = db.Column(db.String(100), nullable=False)
+	price = db.Column(db.Numeric(2, 10), nullable=False)
+
+	def __init__(self, item_id, name, price):
+		self.item_id = item_id
 		self.name = name
 		self.price = price
 
@@ -26,29 +64,77 @@ class Food(db.Model):
 ######### APIs #########
 @app.route('/', methods=['POST', 'GET'])
 def cart():
+	"""
+	get request data from Angular
+	file = customer.component.ts
+	method = submitToBackend()
+	"""
 	request_data = request.get_json()
-	print(request_data)
+
 
 	if request.method == 'POST':
-		# get data from json data and put into local vars
-		name = request_data['name']
-		price = request_data['price']
-		print(request_data)
-		updated_msg = f'The database has been updated with {name} as the name and {price} as the price.'
+		if request_data is not None:
+			"""
+			We must first initialize the order in the 'orders' table on the
+			mysql db. Then we retrieve the 'order_id' that was generated for
+			the current order. We must do this instead of creating the 'order_id'
+			on the backend because the 'order_id' is auto_incremented
+			"""
+			# initialize the order and add it to the 'orders' table in mysql db
+			order_entry = Orders(datetime.now())
+			db.session.add(order_entry)
+			db.session.commit()
 
-		# create new Food entry from local vars
-		menu_item = Food(name, price)
+			# get the 'order_id' from the database
+			current_order_id = Orders.query.order_by(Orders.order_id.desc()).first()
+			print(current_order_id)
 
-		# intitializes table
-		# db.create_all()
-		
-		# commit entry to database
-		db.session.add(menu_item)
-		db.session.commit()
+			for i in request_data:
+				order_content = OrderItems(
+					i['id'],			# order_item_id
+					current_order_id,	# order_id
+					i['name'],			# name
+					i['price'])			# price
+				db.session.add(order_content)
+			
+			db.session.commit()
 
-		return render_template('flask.html', msg=updated_msg)
+		return render_template('flask.html', msg=request_data)
 	else:
 		return render_template('flask.html', msg='This is a GET')
+
+
+
+
+
+
+
+
+
+
+	
+
+	# if request.method == 'POST':
+	# 	# get data from json data and put into local vars
+	# 	item_id = request_data['id']
+	# 	name = request_data['name']
+	# 	price = request_data['price']
+	# 	print(request_data)
+	# 	updated_msg = f'The database has been updated with {name} as the name and {price} as the price.'
+
+	# 	# create new Food entry from local vars
+	# 	menu_item = Items(item_id, name, price)
+
+	# 	# intitializes table
+	# 	db.create_all()
+		
+	# 	# commit entry to database
+	# 	db.session.add(menu_item)
+	# 	db.session.commit()
+
+	# 	return render_template('flask.html', msg=updated_msg)
+	# else:
+	# 	return render_template('flask.html', msg='This is a GET')
 
 
 
