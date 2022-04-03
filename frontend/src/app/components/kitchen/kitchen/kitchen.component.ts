dd0aa85fe7f4abf73cce3e203fe1cf7f18272441
TomReilly1 <reilly.thomas.c@gmail.com>
2022-04-03 16:51:27 -0400

    adding stripe webhook

--- frontend/src/app/components/kitchen/kitchen/kitchen.component.ts
+++ frontend/src/app/components/kitchen/kitchen/kitchen.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnDestroy, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-kitchen',
   templateUrl: './kitchen.component.html',
   styleUrls: ['./kitchen.component.css']
 })
-export class KitchenComponent implements OnInit {
+export class KitchenComponent implements OnInit,OnDestroy {
+  // timeInterval: Subscription;
+  title = 'KITCHEN COMPONENT';
+  status: any;
+
+
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+    // this.timeInterval = interval(5000)
+    // .pipe(
+    //   startWith(0),
+    //   switchMap(() => this.)
+    // )
+  }
+
+  ngOnDestroy(): void {
+    
   }
 
 }
