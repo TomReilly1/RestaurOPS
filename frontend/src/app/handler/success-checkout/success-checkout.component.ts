dbe6b716d5dfc4aa39ce8d1fd2baa6d09a3b3e23
Nick Sloss <nick@improvation.us>
2022-05-02 18:10:18 -0400

    update kitchen

--- frontend/src/app/handler/success-checkout/success-checkout.component.ts
+++ frontend/src/app/handler/success-checkout/success-checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success-checkout',
@@ -7,12 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessCheckoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    let bc = new BroadcastChannel("checkout");
-    bc.postMessage("success");
-    close();
+    this.router.navigateByUrl("/")
   }
 
 }
